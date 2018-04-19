8a0bf005a3499a2d3ce5d31f348f4201187b596d
it7-hageya <it7-hageya@R142-19.BS14.LAN>
2018-04-19 11:48:12 +0200

    Adjusted userarea

--- userarea.php
+++ userarea.php
@@ -1,4 +1,11 @@
 <?php
-echo "Test";
+session_start();
+if(!isset($_SESSION['userid'])) {
+    die('Bitte zuerst <a href="index.php">einloggen</a>');
+}
+ 
+//Abfrage der Nutzer ID vom Login
+$userid = $_SESSION['userid'];
+ 
+echo "Hallo User: ".$userid;
 ?>
-
