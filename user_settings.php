0da7d9b40be1f71a0851e49e52775c8f78be7751
it7-ehleph <it7-ehleph@R142-20.BS14.LAN>
2018-04-19 15:11:56 +0200

    -add user_settings

--- user_settings.php
+++ user_settings.php
@@ -1,6 +1,19 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=itech', 'root');
+session_start();
+if(!isset($_SESSION['userid'])) {
+    die('Bitte zuerst <a href="index.php">einloggen</a>');
+}
+ 
+//Abfrage der Nutzer ID vom Login
+$userid = $_SESSION['userid'];
 
+
+$pdo = new PDO('mysql:host=localhost;dbname=itech', 'root');
+$sql = "SELECT * FROM users WHERE id = $userid";
+foreach ($pdo->query($sql) as $row) {
+    $anzeigename = $row['anzeigename'];
+    $email = $row['email'];
+}
 ?>
 <!DOCTYPE html> 
 <html> 
@@ -19,9 +32,9 @@ $pdo = new PDO('mysql:host=localhost;dbname=itech', 'root');
 <div class="login">
     <form action="?change=1" method="post">
         <label>Anzeigename:</label>
-        <input size="40" maxlength="250" name="anzeigename"/><br>
+        <input size="40" maxlength="250" name="anzeigename" value="<?php echo"$anzeigename"; ?>"/><br>
         <label>E-Mail:</label>
-        <input type="email" size="40" maxlength="250" name="email" value="<?php echo"$email"; ?>"><br><br>
+        <input type="email" size="40" maxlength="250" name="email" value="<?php echo"$email"; ?>"/><br><br>
         <label>Dein Passwort:</label>
         <input type="password" size="40"  maxlength="250" name="passwort"><br>
         <label>Passwort wiederholen:</label>
