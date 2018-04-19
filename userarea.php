111052d553a3bddb1f1a77eb1aa620bdd8414c96
it7-ehleph <it7-ehleph@R142-20.BS14.LAN>
2018-04-19 12:19:23 +0200

    - add dropdown

--- userarea.php
+++ userarea.php
@@ -1,4 +1,18 @@
-<?php
-echo "Test";
-?>
+<!DOCTYPE html> 
+<html> 
+<head>
+  <title>Upload</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css"> 
+</head> 
+<body>
+<select name="usermenu">
+  <option value="Edit">Benutzer Einstellungen</option>
+  <option value="X">WIP</option>
+</select>
 
+<?php
+
+
+?>
+</body>
+</html>
