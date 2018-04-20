1c82be7550df5ace523221c1e40a56bcd8e23726
Yannic <Yannic@DESKTOP-G6VO51N.fritz.box>
2018-04-20 12:58:55 +0200

    config test

--- config/config.inc.php
+++ config/config.inc.php
@@ -2,7 +2,7 @@
 /* INCLUDE: Verbindung zur Text-Datenbank herstellen */
  
 $db_server = 'localhost'; // Hostname
-$db_user = 'roott'; // Benutzername
+$db_user = 'root'; // Benutzername
 $db_pass = ""; // Kennwort
 $db_name = "itech01"; // Name der Datenbank
 $link  = new PDO("mysql:host=$db_server;dbname=$db_name", $db_user, $db_pass);
