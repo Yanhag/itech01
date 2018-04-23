94f1197af97b0ad3a060e4dd41991b6ad5344a6b
it7-ehleph <it7-ehleph@R143-14.BS14.LAN>
2018-04-23 11:45:39 +0200

    -Header mit navi umgesetzt -> TODO: CSS auslagern

--- header.php
+++ header.php
@@ -4,20 +4,86 @@ if(!isset($_SESSION['userid'])) {
 }
 ?>
 
+
 <!DOCTYPE html>
-<html> 
+<html>
 <head>
-  <title>Upload</title>
-  <link rel="stylesheet" type="text/css" href="css/main.css"> 
-</head> 
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a, .dropbtn {
+    display: inline-block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+    background-color: red;
+}
+
+li.dropdown {
+    display: inline-block;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+</style>
+</head>
 <body>
-<select name="usermenu" onchange="location = this.value;">
-  <option selected disabled>Menu</option>
-  <option value="userarea.php">Startsteite</option>
-  <option value="user_settings.php">Benutzer Einstellungen</option>
-  <option value="X">WIP</option>
-</select>
-<a href="upload.php">Upload</a>
-<a href="logout.php">Logout</a>
+
+<ul>
+  <li><a href="userarea.php">Home</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Durchsuchen</a>
+    <div class="dropdown-content">
+      <a href="#">Neu</a>
+      <a href="#">Beliebt</a>
+    </div>
+  </li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Benutzer</a>
+    <div class="dropdown-content">
+      <a href="user_settings.php">Benutzerdaten ändern</a>
+      <a href="#">Passwort ändern</a>
+    </div>
+  </li>
+  <li><a href="upload.php">Upload</a></li>
+  <li><a href="logout.php">Logout</a></li>
+</ul>
 <br>
 <br>
+</body>
+</html>
+
